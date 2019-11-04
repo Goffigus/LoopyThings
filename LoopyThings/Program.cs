d31709b64b9d7fc595944a21f72884aaf3d662fc
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-04 09:20:25 -0500

    Add project files.

---
+++ LoopyThings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace LoopyThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int minutes = 0;  //Total number of Minutes
+            string input; //user input
+            bool run = true;
+
+            while (run) //run until user puts in non-integer
+            {
+                input = ReadLine(); //gets user input
+
+                //checks to see input can be converted to an integer and sets minutes to the user input
+                run = Int32.TryParse(input, out minutes);
+
+                if (run) //if user input is an integer do math
+                {
+                    int hours = minutes / 60; //Number of hours in the minutes
+                    int rMinutes = minutes % 60; //Remainder minutes
+
+                    WriteLine(minutes + " minutes is " +
+                        hours + " hours and " + rMinutes + " minutes.");
+                }
+                else 
+                {
+                    WriteLine("exiting");
+                }
+            }
+
+        }
+    }
+}
